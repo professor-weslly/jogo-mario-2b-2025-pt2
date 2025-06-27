bf938409b397075f1974790018e805026a78908d
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2025-06-27 07:18:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const mario = document.querySelector(".mario")
 const pipe = document.querySelector(".pipe")
+const nuvem = document.querySelector(".nuvem")
 const startButton = document.querySelector(".start")
 const gameOverScreen = document.querySelector(".game-over")
 
@@ -11,6 +12,9 @@ const startGame = () => {
     pipe.style.animation = "pipe-animation 1.5s infinite linear";
 
     startButton.style.display = "none";
+    mario.style.opacity = "1";
+    pipe.style.opacity = "1";
+    nuvem.style.opacity = "1";
 }
 
 
@@ -39,6 +43,9 @@ const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
         mario.style.marginLeft = "50px";
 
         clearInterval(loop);
+        gameOverScreen.style.display = "flex";
+    } else if (pipePosition < 0 && gameStarted) {
+     pipe.style.left = '';   
     }
 },10);
 
