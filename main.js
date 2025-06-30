67db05b9221f64aefa2ad05afb7fa542a6a53d24
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2025-06-30 07:39:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ const pipe = document.querySelector(".pipe")
 const nuvem = document.querySelector(".nuvem")
 const startButton = document.querySelector(".start")
 const gameOverScreen = document.querySelector(".game-over")
-
+const scoreElement = document.querySelector(".score")
 
 audioStart = new Audio("./sound/audio_theme.mp3");
 const gameOverSound = new Audio("./sound/audio_gameover.mp3");
 
 let gameStarted = false;
+let score = 0;
 
 const startGame = () => {
     gameStarted = true;
@@ -32,6 +33,13 @@ const jump = () => {
     ,500);
 }
 
+const updateScore = () => {
+    score += 1;
+    scoreElement.textContent = score;
+    const animationSpeed  = 1.5/ (1 + score/500);
+    pipe.style.animation = `pipe-animation ${animationSpeed}s infinite linear`;
+}
+
 const loop = setInterval(() => {
 const pipePosition = pipe.offsetLeft;
 const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
@@ -53,6 +61,7 @@ const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
         clearInterval(loop);
         gameOverScreen.style.display = "flex";
     } else if (pipePosition < 0 && gameStarted) {
+     updateScore();   
      pipe.style.left = '';   
     }
 },10);
